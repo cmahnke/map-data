f5534424e301fa8e6efec3edfac7d9943bbcb845
Christian Mahnke <cmahnke@gmail.com>
2023-09-25 18:11:10 +0200

    keep more buildings

--- docker/data/scripts/extract-buildings.sh
+++ docker/data/scripts/extract-buildings.sh
@@ -4,7 +4,7 @@ if test -n "$1" ; then
   OUT="$1-extracted.osm.pbf"
   #echo "Extracting remaining buildings from $1" && \
   /opt/osmctools/bin/osmconvert $1 -o=$1.o5m && \
-  /opt/osmctools/bin/osmfilter $1.o5m --keep='building=yes and historic=*' --keep='building=yes and name=*' --keep='building=yes and tourism=artwork' --keep='building:architecture=Brick\ Expressionism' |/opt/osmctools/bin/osmconvert - -o=$OUT
+  /opt/osmctools/bin/osmfilter $1.o5m --keep='building=yes and historic=*' --keep='building=yes and name=*' --keep='building=yes and tourism=artwork' --keep='building=yes and architect*=*' --keep='building:architecture=*' |/opt/osmctools/bin/osmconvert - -o=$OUT
   rm $1.o5m
   echo "$OUT"
 fi
