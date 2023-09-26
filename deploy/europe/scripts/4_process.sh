adc9f6be76f33810448f96dc5cc31bef42194360
Christian Mahnke <cmahnke@gmail.com>
2023-09-26 23:09:28 +0200

    Don't merge buildings

--- deploy/europe/scripts/4_process.sh
+++ deploy/europe/scripts/4_process.sh
@@ -13,7 +13,7 @@ if [ -z "$TILES" ] ; then
 fi
 
 echo "Starting Planetiler" && \
-$PLANETILER --download=false --languages=de,en --use_wikidata=false --osm-path=$POSTPROCESSED --free_water_polygons_after_read=true --free_natural_earth_after_read=true --free_osm_after_read=true --free_lake_centerlines_after_read=true --tile_compression=${TILE_COMPRESSION} --maxzoom=${MAX_ZOOM} --render_maxzoom=${MAX_ZOOM} --exclude-layers=${EXCLUDE_LAYERS} $PLANETILER_OPTS
+$PLANETILER --download=false --languages=de,en --use_wikidata=false --osm-path=$POSTPROCESSED --free_water_polygons_after_read=true --free_natural_earth_after_read=true --free_osm_after_read=true --free_lake_centerlines_after_read=true --tile_compression=${TILE_COMPRESSION} --maxzoom=${MAX_ZOOM} --render_maxzoom=${MAX_ZOOM} --building_merge_z13=false --exclude-layers=${EXCLUDE_LAYERS} $PLANETILER_OPTS
 
 rm -f $POSTPROCESSED
 $MBUTIL --silent --image_format=pbf data/output.mbtiles $TILES
