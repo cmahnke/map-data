1deb110e48954b983e2fa2a645a76a518577080f
Christian Mahnke <cmahnke@gmail.com>
2023-09-28 19:52:51 +0200

    Switch building extraction to common config

--- deploy/docker/data/scripts/extract-buildings.sh
+++ deploy/docker/data/scripts/extract-buildings.sh
@@ -4,7 +4,9 @@ if test -n "$1" ; then
   OUT="$1-extracted.osm.pbf"
   #echo "Extracting remaining buildings from $1" && \
   /opt/osmctools/bin/osmconvert $1 -o=$1.o5m && \
-  /opt/osmctools/bin/osmfilter $1.o5m --keep='building=yes and historic=*' --keep='building=yes and name=*' --keep='building=yes and tourism=artwork' --keep='building=yes and architect*=*' --keep='building:architecture=*' |/opt/osmctools/bin/osmconvert - -o=$OUT
+  #/opt/osmctools/bin/osmfilter $1.o5m --keep='building=yes and historic=*' --keep='building=yes and name=*' --keep='building=yes and tourism=artwork' --keep='building=yes and architect*=*' --keep='building:architecture=*' |/opt/osmctools/bin/osmconvert - -o=$OUT
+  /opt/osmctools/bin/osmfilter $1.o5m --parameter-file=../config/osmfilter-buildings |/opt/osmctools/bin/osmconvert - -o=$OUT
+
   rm $1.o5m
   echo "$OUT"
 fi
